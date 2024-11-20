0296099eec0ff9d386ac49e2eb8258abe632e456
Gabriel Batista <gabriel_batista@outlook.com>
2024-11-20 20:57:59 +0000

    chore: fix files using `prettier-maven-plugin`

--- LambdaUrlCreator/src/main/java/com/bobathief/UrlShortener/Main.java
+++ LambdaUrlCreator/src/main/java/com/bobathief/UrlShortener/Main.java
@@ -1,35 +1,35 @@
 package com.bobathief.UrlShortener;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
-public class Main implements RequestHandler<Map<String, Object>, Map<String, String>>{
-    private final ObjectMapper objectMapper = new ObjectMapper();
+	private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Override
-    public Map<String, String> handleRequest(Map<String, Object> input, Context context){
-        String body = input.get("body").toString();
+	@Override
+	public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+		String body = input.get("body").toString();
 
-        Map<String, String> bodyMap;
-        try {
-            bodyMap = objectMapper.readValue(body, Map.class);
-        } catch (Exception exception) {
-            throw new RuntimeException("Error parsing JSON body: "+exception.getMessage(), exception);
-        }
+		Map<String, String> bodyMap;
+		try {
+			bodyMap = objectMapper.readValue(body, Map.class);
+		} catch (Exception exception) {
+			throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
+		}
 
-        String originalUrl = bodyMap.get("originalUrl");
-        String expirationTime = bodyMap.get("expirationTime");
+		String originalUrl = bodyMap.get("originalUrl");
+		String expirationTime = bodyMap.get("expirationTime");
 
-        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+		String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
 
-        Map<String, String> response = new HashMap<>();
-        response.put("urlCode", shortUrlCode);
+		Map<String, String> response = new HashMap<>();
+		response.put("urlCode", shortUrlCode);
 
-        return response;
-    }
+		return response;
+	}
 }
