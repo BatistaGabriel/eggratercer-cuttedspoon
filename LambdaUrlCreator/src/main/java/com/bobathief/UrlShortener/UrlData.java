ecc60049d5eb90f84f653d97a808716f72e0d34b
Gabriel Batista <gabriel_batista@outlook.com>
2024-11-21 01:09:09 +0000

    fix: fix deserialization bug on aws

--- LambdaUrlCreator/src/main/java/com/bobathief/UrlShortener/UrlData.java
+++ LambdaUrlCreator/src/main/java/com/bobathief/UrlShortener/UrlData.java
@@ -1,10 +1,12 @@
 package com.bobathief.UrlShortener;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 public class UrlData {
